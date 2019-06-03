63f48f638ae091601980b479cfc9185cb25ba442
czarniak97 <51341140+czarniak97@users.noreply.github.com>
2019-06-03 23:22:48 +0200

    Update app.js
    poprawki na js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 app = document.getElementById("app");
 app.innerHTML = "APLIKACJA";
 app.style.color = "blue";
-app.style.background-color = "red";
-app.style.font-weight = "500";
-app.style.font-size = "30px";
+app.style.backgroundColor = "red";
+app.style.fontWeight = "500";
+app.style.fontSize = "30px";
 app.style.width = "75vw";
 app.style.height ="75vh";
-app.style.margin-top = "10vh";
-app.style.margin-left = "10vw";
+app.style.marginTop = "10vh";
+app.style.marginLeft = "10vw";
